2f7953caab8fb55ac4bb9615a2ad063dc0961a81
Renato Souza <renatobruno134@hotmail.com>
2022-06-28 18:43:00 -0300

    Desenvolvendo o Layout da Tela de Login

---
+++ src/view/login/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './login.css'
+
+function Login() {
+    return (
+        <div className='login-content d-flex align-items-center'>
+            <form className="form-signin mx-auto">
+                <div className='text-center mb-4'>
+                    <h1 className="h3 mb-3 font-weight-normal text-white font-weight-bold">Login</h1>
+                </div>
+                <input type="email" id="inputEmail" className="form-control my-2" placeholder="Email" />
+                <input type="password" id="inputPassword" className="form-control my-2" placeholder="Senha" />
+                <button className="btn btn-lg btn-block btn-login my-2" type="submit">Sign in</button>
+                <div className='msg-login text-white text-center my-5'>
+                    <span><strong>WOW! </strong>Voce esta conectado! &#128526;</span>
+                    <br></br>
+                    <span><strong>Ops! </strong>Verifique se a senha ou usuario estao corretos! &#128546;</span>
+                </div>
+                <div className='opcoes-login mt-5 text-center'>
+                    <a href='#' className='mx-2'>Recuperar senha</a>
+                    <span className='text-white'>&#9733;</span>
+                    <a href='#' className='mx-2'>Quero Cadastrar</a>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Login
